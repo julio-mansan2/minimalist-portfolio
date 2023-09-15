87b9ba4ac9989e4057316f851456b8ecb7e6f637
julio-mansan2 <110508390+julio-mansan2@users.noreply.github.com>
2023-09-15 23:47:01 +0000

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -39,6 +39,7 @@ const btnLeft = document.querySelectorAll('.btn-left');
 const btnRight = document.querySelectorAll('.btn-right');
 
 function updatePages(selectedPage, selectedLabelColor) {
+
   for (const pageName in pages) {
     if (pages.hasOwnProperty(pageName)) {
       const page = pages[pageName];
@@ -99,16 +100,72 @@ handleButton(buttons.bookmark, pages.bookmark, 'var(--slightlyDesaturatedCyan)')
 handleButton(buttons.insure, pages.insure, 'var(--slightlyDesaturatedCyan)');
 handleButton(buttons.fylo, pages.fylo, 'var(--slightlyDesaturatedCyan)');
 
-// Handle btnLeft and btnRight
-for (let i = 0; i < btnLeft.length; i++) {
-  btnLeft[i].addEventListener('click', function () {
-    updatePages(pages.fylo, 'var(--slightlyDesaturatedCyan)');
-  });
 
-  btnRight[i].addEventListener('click', function () {
-    updatePages(pages.fylo, 'var(--slightlyDesaturatedCyan)');
-  });
-}
+btnLeft[0].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.add('selecionado')
+})
+
+btnLeft[1].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.add('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
+
+btnLeft[2].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.add('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
+
+btnLeft[3].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.add('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
+
+// Sliders - Btn Right
+
+btnRight[0].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.add('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
+
+btnRight[1].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.add('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
+
+btnRight[2].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.remove('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.add('selecionado')
+})
+
+btnRight[3].addEventListener('click', function () {
+  portfolioPage.classList.remove('selecionado')
+  managePage.classList.add('selecionado')
+  bookmarkPage.classList.remove('selecionado')
+  insurePage.classList.remove('selecionado')
+  fyloPage.classList.remove('selecionado')
+})
 
 const error = document.querySelectorAll('.error')
 const inputEmail = document.querySelector('#email')
@@ -117,34 +174,35 @@ const textArea = document.querySelector('#textarea')
 const inputSubmit = document.querySelector('.submit')
 
 inputSubmit.addEventListener('click', function () {
-    event.preventDefault()
-    if (inputName.value === '') {
-        error[0].innerHTML = 'Please, insert a name'
-    } else {
-        error[0].innerHTML = ''
-    }
+  event.preventDefault()
+  if (inputName.value === '') {
+    error[0].innerHTML = 'Please, insert a name'
+  } else {
+    error[0].innerHTML = ''
+  }
 
-    if (inputEmail.validity.valid === false || inputEmail.value === '') {
-        error[1].innerHTML = 'Please, insert a valid email'
-    } else {
-        error[1].innerHTML = ''
-    }
+  if (inputEmail.validity.valid === false || inputEmail.value === '') {
+    error[1].innerHTML = 'Please, insert a valid email'
+  } else {
+    error[1].innerHTML = ''
+  }
 
-    if (textArea.value === '') {
-        error[2].innerHTML = 'Please, insert a message'
-    } else {
-        error[2].innerHTML = ''
-    }
+  if (textArea.value === '') {
+    error[2].innerHTML = 'Please, insert a message'
+  } else {
+    error[2].innerHTML = ''
+  }
 })
 
 const menuHamburguer = document.getElementById('menu__toggle')
 const navMenu = document.querySelector('.menu__box')
 
 menuHamburguer.addEventListener('click', function () {
-    if (menuHamburguer.checked) {
-        navMenu.classList.add('aparecer')
-    } else {
-        navMenu.classList.remove('aparecer')
-        navMenu.classList.add('ocultar')
-    }
+  if (menuHamburguer.checked) {
+    navMenu.classList.add('aparecer')
+  } else {
+    navMenu.classList.remove('aparecer')
+    navMenu.classList.add('ocultar')
+  }
 })
+
